065e26c426a309d76a818253eb7a973c55116137
Kateryna <Kateryna@DESKTOP-8FKIP79>
2017-01-18 21:34:18 +0200

    Add project files.

---
+++ ConsoleApplication24/FileWatcher.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication24
+{
+    public class FileWatcher
+    {
+        public object eventLocker = new object();
+
+        public event Action OnWatch;
+
+        public object lockObject = new object();
+
+        public string FilePath;
+
+        private DateTime CurrentTime { get; set; } = DateTime.Now;
+
+        public FileWatcher(string path)
+        {
+            FilePath = path;
+        }
+
+        public void Start()
+        {
+            Thread t = new Thread(Watch);
+            t.Start();
+        }
+
+        public void Watch()
+        {
+            lock (lockObject)
+            {
+                while (true)
+                {
+                    var writeTime = File.GetLastWriteTime(FilePath);
+                    if (writeTime > CurrentTime)
+                    {
+                        CurrentTime = writeTime;
+                        OnWatching();
+                    }
+                }
+            }
+        }
+
+        protected virtual void OnWatching()
+        {
+            Action handler;
+            lock (lockObject)
+            {
+                handler = OnWatch;
+            }
+            if (handler != null)
+            {
+                handler();
+            }
+        }
+    }
+}
---
+++ ConsoleApplication24/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = "test.txt";
+            var file = File.Create(path);
+            var watcher = new FileWatcher(path);
+            file.Close();
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                sw.Write("1");
+                
+            }
+            watcher.OnWatch += FileChanged;
+            watcher.Start();
+        }
+
+        public static void  FileChanged()
+        {
+            Console.WriteLine("The file was changed");
+            if (string.Equals(File.ReadAllText("test.txt"), "1", StringComparison.OrdinalIgnoreCase))
+            {
+                File.WriteAllText("test.txt", "0");
+                Thread.Sleep(3000);
+            }
+            
+        }
+    }
+}
